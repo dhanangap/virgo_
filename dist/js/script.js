87671c5e292e26cfdfd5dc9070111a8a4b684c78
Dhanang <dhanangap@outlook.com>
2022-01-01 03:26:21 +0700

    prepare

---
+++ dist/js/script.js
@@ -0,0 +1,23 @@
+import { Button } from "./_button.js";
+import { Form } from "./_form.js";
+import { Navbar } from "./_navbar.js";
+import { Slider } from "./_slider.js";
+import { Modal } from "./_modal.js";
+import { Tab } from "./_tab.js";
+import { Tagger } from "./_tagger.js";
+import { Carousel } from "./_carousel.js";
+import { Gallery } from "./_gallery.js";
+
+window.onload = () => {
+    // Form.initForms();
+    // Button.initDropdownButtons();
+    // Navbar.initNavbar();
+    Slider.init();
+    Modal.init();
+    Tab.init();
+    Tagger.init();
+    Carousel.init();
+    Gallery.init();
+};
+
+window.Modal = Modal;
