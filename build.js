d61c47912b207d4d645847791ca0497ef594d18a
Dhanang <dhanangap@outlook.com>
2022-01-22 03:39:06 +0700

    add style to build process

--- build.js
+++ build.js
@@ -19,22 +19,6 @@ const config = {
 	sourceDir: "./src",
 	outputDir: "./dist",
 
-	components: [
-		"accordion",
-		"carousel",
-		"gallery",
-		"map",
-		"modal",
-		"nav",
-		"navbar",
-		"preloader",
-		"rating",
-		"slider",
-		"stack",
-		"tab",
-		"tagger",
-	],
-
 	styles: [
 		"base",
 		"layout",
@@ -46,36 +30,6 @@ const config = {
 };
 
 try {
-
-	const componentsOutDir = config.outputDir + "/components";
-	if (!fs.existsSync(componentsOutDir)) fs.mkdirSync(componentsOutDir);
-
-	config.components.forEach(component => {
-		const outDir = componentsOutDir + "/" + component;
-
-		if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
-
-		// Compile style
-		const styleInput = config.sourceDir + "/components/" + component + "/" + component + ".scss";
-		const styleOutput = outDir + "/" + component + ".min.css";
-		console.log(`Compiling: ${styleInput}`);
-		if (fs.existsSync(styleInput)) {
-			const css = sass.renderSync({ file: styleInput });
-			postcss([cssnano, autoprefixer]).process(css.css.toString()).then(result => {
-				fs.writeFileSync(styleOutput, result.css);
-			})
-		}
-
-		// Compile scripts
-		const jsInput = config.sourceDir + "/components/" + component + "/" + component + ".js";
-		const jsOutput = outDir + "/" + component + ".min.js";
-		console.log(`Compiling: ${jsInput}`);
-		if (fs.existsSync(jsInput)) {
-			const js = fs.readFileSync(jsInput, "utf8");
-			fs.writeFileSync(jsOutput, uglify.minify(js).code);
-		}
-
-	});
 
 	config.styles.forEach(style => {
 		const styleInput = config.sourceDir + "/" + style + ".scss";
