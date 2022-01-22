c0563554ddceaadb18b6a36238c8c3e03348be18
Dhanang <dhanangap@outlook.com>
2022-01-22 17:54:09 +0700

    add show effect

--- src/animation/show.ts
+++ src/animation/show.ts
@@ -9,3 +9,12 @@ export function hide (element: HTMLElement | Element, callback?: Function) : voi
 	target.style.display = "none";
 	if (callback) callback();
 }
+
+/** Show an element */
+export function show (element: HTMLElement | Element, callback?: Function) : void {
+	const target = element as HTMLElement;
+	if (target.style.display === "none")		target.style.display 	= "block";
+	if (parseFloat(target.style.opacity) === 0)	target.style.opacity 	= "1";
+	if (parseFloat(target.style.height) === 0)	target.style.height 	= "auto";
+	if (callback) callback();
+}
