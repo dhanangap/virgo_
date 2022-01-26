802d0e40c73a8a0447d5bee8f4b15451716ead18
Dhanang <dhanangap@outlook.com>
2022-01-26 16:09:05 +0700

    dropdown component update

--- src/components/Dropdown/DropdownButton/DropdownButton.ts
+++ src/components/Dropdown/DropdownButton/DropdownButton.ts
@@ -1,5 +1,18 @@
 import Component from "../../Base/Component";
+import DropdownComponent from "../DropdownComponent/DropdownComponent";
 
 export default class DropdownButton extends Component {
+	
+	parent?: DropdownComponent;
+
+	constructor(element: HTMLElement | Element, config?: any) {
+		super(element, config);
+		if (config) {
+			this.parent = config.parent;
+		}
+		this.element.addEventListener("click", () => {
+			this.parent.toggleOpen();
+		});
+	}
 
 }
--- src/components/Dropdown/DropdownComponent/DropdownComponent.ts
+++ src/components/Dropdown/DropdownComponent/DropdownComponent.ts
@@ -1,4 +1,5 @@
 import Component from "../../Base/Component";
+import ComponentInterface from "../../base/ComponentInterface";
 import DropdownButton from "../DropdownButton/DropdownButton";
 import DropdownMenu from "../DropdownMenu/DropdownMenu";
 
@@ -8,18 +9,71 @@ export default class DropdownComponent extends Component {
 
 	static className: string = "dropdown";
 
+	static init(selector?: string, config?: ComponentInterface): void {
+		super.init(selector, config);
+		document.addEventListener("keydown", (event) => {
+			if (event.key === "Escape") {
+				this.closeAll();
+			}
+		});
+		document.addEventListener("click", (event) => {
+			if (!this.isInsideDropdown(event.target)) {
+				this.closeAll();
+			}
+		});
+	}
+
+	static closeAll () : void {
+		for (const dropdown of this.instances) {
+			(dropdown as DropdownComponent).close();
+		}
+	}
+
+	static isInsideDropdown (clickedElement: EventTarget) : boolean {
+		let value = false;
+		let i = 0;
+		
+		while (value === false && i < this.instances.length) {
+			const dropdown = this.instances[i] as DropdownComponent;
+			value = dropdown.element.contains(clickedElement as Node);
+			i++;
+		}
+
+		return value;
+	}
+
 	// * Object Properties and Methods
 
 	transition: string = "none";
 	button?: DropdownButton;
 	menu?: DropdownMenu;
 	
+	get isOpen () : boolean {
+		return this.element.dataset["state"] === "open";
+	}
+
+	set isOpen (value: boolean) {
+		if (value) {
+			this.element.dataset["state"] = "open";
+		} else {
+			this.element.removeAttribute("data-state");
+		}
+	}
+	
 	constructor(element: HTMLElement | Element, config?: any) {
 		super(element, config);
 		if (config) this.transition = config.transition ? config.transition : "none";
 
-		this.button = new DropdownButton(this.element.querySelector("button"));
-		this.menu = new DropdownMenu(this.element.querySelector(".menu"));
+		this.button = new DropdownButton(this.element.querySelector("button"), { parent: this });
+		this.menu = new DropdownMenu(this.element.querySelector(".menu"), { parent: this });
+	}
+
+	toggleOpen () : void {
+		this.isOpen = !this.isOpen;
+	}
+
+	close () : void {
+		this.isOpen = false;
 	}
 
 }
